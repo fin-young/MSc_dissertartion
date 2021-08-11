79919a025610288816ff3119d02ccf3bd80b04ab
fin_young <fin.young92@gmail.com>
2021-08-11 23:45:58 +0100

    400epc trial does not converge. Push to deepnote

--- FX_DATA_GEN.py
+++ FX_DATA_GEN.py
@@ -736,10 +736,11 @@ def Get_FX_MacEcon_Data(iso2):
     fx_field = dataset.iloc[:,0].name
     roll30 = fx_field+'_30_ave'
     roll60 = fx_field+'_60_ave'
+    roll90 = fx_field+'_90_ave'
     dataset[roll30] = dataset[fx_field].rolling(30).mean()
     dataset[roll60] = dataset[fx_field].rolling(60).mean()
-    dataset[fx_field] = dataset[fx_field].rolling(90).mean()
-    dataset.dropna(axis=0, subset=[fx_field], inplace = True)
+    dataset[roll90] = dataset[fx_field].rolling(90).mean()
+    dataset.dropna(axis=0, subset=[roll90], inplace = True)
     return dataset
 
 def describe_MacEcon_TS(df):
